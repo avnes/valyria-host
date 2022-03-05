78794ae29ffa88750a4f081d4776aa2c13574fdb
Audun Nes <audun.nes@gmail.com>
2022-03-05 20:09:22 +0100

    Add missing packages

--- bin/01_run_ansible.sh
+++ bin/01_run_ansible.sh
@@ -5,7 +5,7 @@ cd "${0%/*}/.."
 
 printf "\n\n############### Installing Linux software requirements ###############\n"
 
-sudo dnf install -y python39 curl
+sudo dnf install -y python39 curl python3-libselinux python39-devel
 
 printf "\n\n############### Installing Poetry ###############\n"
 curl -sSL https://install.python-poetry.org | python3 -
@@ -14,5 +14,5 @@ PATH=$HOME/.local/bin:$PATH; export PATH
 printf "\n\n############### Installing Ansible ###############\n"
 poetry install
 
-printf "\n\n############### Installing CLI tools ###############\n"
+printf "\n\n############### Run Ansible ###############\n"
 poetry run ansible-playbook --inventory ansible/inventory/hosts.yaml ansible/playbook.yaml
